3a085f2d2a41b7daed4f1bc951f9c5138200d455
Matt Gregory <mgregory22@gmail.com>
2017-02-05 10:32:16 -0500

    Add project files.

---
+++ MSGTest/Types/String/FormatTests.cs
@@ -0,0 +1,25 @@
+﻿//
+// MSGTest/Types/String/FormatTests.cs
+//
+
+using MSG.Types.String;
+using NUnit.Framework;
+
+namespace MSGTest.Types.String
+{
+    [TestFixture]
+    public class FormatTests
+    {
+        [Test]
+        public void TestToLiteral()
+        {
+            Assert.AreEqual("\\t\\n", Format.ToLiteral("\t\n"));
+        }
+
+        [Test]
+        public void TestPadding()
+        {
+            Assert.AreEqual("   ", Format.Padding(3));
+        }
+    }
+}
